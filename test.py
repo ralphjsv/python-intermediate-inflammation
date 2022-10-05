fa44ed96b0781db8443989473ce81ea0e4c40fcc
Ralph Julius Sulit Velasquez <r.j.sulitvelsquez@student.tudelft.nl>
2022-10-05 10:20:47 +0200

    Adding a Fahrenheit to Celsius function.

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-if (a == True and
-    b == False):
+# if (a == True and
+#     b == False):
 
-income = (gross_wages
-          + taxable_interest
-          + (dividends - qualified_dividends)
-          - ira_reduction
-          - student_loan_interest)
+# income = (gross_wages
+#           + taxable_interest
+#           + (dividends - qualified_dividends)
+#           - ira_reduction
+#           - student_loan_interest)
+
+def fahr_to_cels(fahr):
+    # Convert temperature in Fahrenheit to Celsius
+    cels = (fahr + 32) * (5/9)
+    return cels
