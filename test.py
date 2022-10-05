0d14563455b253cac7eecb9f261cc5b3b76e79de
Ralph Julius Sulit Velasquez <r.j.sulitvelsquez@student.tudelft.nl>
2022-10-05 10:13:48 +0200

    Adding a python test file.

---
+++ test.py
@@ -0,0 +1,8 @@
+if (a == True and
+    b == False):
+
+income = (gross_wages
+          + taxable_interest
+          + (dividends - qualified_dividends)
+          - ira_reduction
+          - student_loan_interest)
